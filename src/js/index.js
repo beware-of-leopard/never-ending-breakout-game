fd0960954116ea6718cfee74e32eef40eb2b1b95
DANIEL REVO <DANIELREVO08@GMAIL.COM>
2016-03-27 19:35:43 -0400

    heavily modified the game. Ball no longer starts the same direction each time. Added buffer area to paddle to increase user margin for error. Added levels. When new level is reached, an additional row of bricks is created (level 2 = 2 row of bricks etc) and the speed of the ball is increased by a multiplier.

--- src/js/index.js
+++ src/js/index.js
@@ -4,14 +4,31 @@ var ctx = canvas.getContext("2d");
 var ballRadius = 10;
 var x = canvas.width/2;
 var y = canvas.height-30;
-var dx = 2;
-var dy = -2;
+
+
+/////sends the ball off in two different directions at start
+function pickXDirection(){
+    var speeds = [2.5, -2.5];
+    if (Math.random() < 0.5){
+                return speeds[0];
+            }else{
+                return speeds[1];
+            }
+        };
+
+var dx = pickXDirection();
+
+///y doesn't need a random start direction. Ball starts right above the paddle-would be a very short game half the time.
+var dy = -3;
 var paddleHeight = 10;
 var paddleWidth = 75;
 var paddleX = (canvas.width-paddleWidth)/2;
+
+////buffer eliminates the frustration I always have with these games where I am 100% certain the ball hit the corner of the paddle
+var buffer = 8;
 var rightPressed = false;
 var leftPressed = false;
-var brickRowCount = 3;
+var brickRowCount = 1;
 var brickColumnCount = 5;
 var brickWidth = 75;
 var brickHeight = 20;
@@ -19,7 +36,10 @@ var brickPadding = 10;
 var brickOffsetTop = 60;
 var brickOffsetLeft = 40;
 var score = 0;
+var reset = 0;
+var level = 1;
 
+/////builds the rows and columns of bricks
 var bricks = [];
 for(var c=0; c<brickColumnCount; c++) {
     bricks[c] = [];
@@ -51,14 +71,14 @@ function keyUpHandler(e) {
 function drawBall() {
     ctx.beginPath();
     ctx.arc(x, y, ballRadius, 0, Math.PI*2);
-    ctx.fillStyle = "#0095DD";
+    ctx.fillStyle = "#F28328";
     ctx.fill();
     ctx.closePath();
 }
 function drawPaddle() {
     ctx.beginPath();
     ctx.rect(paddleX, canvas.height-paddleHeight, paddleWidth, paddleHeight);
-    ctx.fillStyle = "#0095DD";
+    ctx.fillStyle = "#F28328";
     ctx.fill();
     ctx.closePath();
 }
@@ -89,9 +109,23 @@ function collisionDetection() {
                     dy = -dy;
                     b.status = 0;
                     score++;
-                    if(score == brickRowCount*brickColumnCount) {
-                        alert("YOU WIN, CONGRATULATIONS!");
-                        document.location.reload();
+                    reset++;
+                    if(reset == brickRowCount*brickColumnCount) {
+
+                        /////adds another row of bricks and multiplies ball speed every time the players beats a level
+                        reset = 0;
+                        dx *= 1.1;
+                        dy *= 1.1;
+                        level++;
+                        brickRowCount++;
+                        bricks = [];
+                        for(var c=0; c<brickColumnCount; c++) {
+                            bricks[c] = [];
+                            for(var r=0; r<brickRowCount; r++) {
+                                bricks[c][r] = { x: 0, y: 0, status: 1 };
+                            }
+                        }
+
                     }
                 }
             }
@@ -103,6 +137,7 @@ function drawScore() {
     ctx.font = "16px Arial";
     ctx.fillStyle = "#0095DD";
     ctx.fillText("Score: "+score, 8, 20);
+    ctx.fillText("Level: "+level, canvas.width - 80, 20);
 }
 
 function draw() {
@@ -120,14 +155,15 @@ function draw() {
         dy = -dy;
     }
     else if(y + dy > canvas.height-ballRadius) {
-        if(x > paddleX && x < paddleX + paddleWidth) {
+        if(x > paddleX - buffer && x < paddleX + paddleWidth + buffer) {
            if(y= y-paddleHeight){
             dy = -dy  ;
 			 }
         }
         else {
             console.log("GAME OVER");
-            document.location.reload();
+            clearInterval(runningTheGame);
+            window.location.reload(true);
         }
     }
     
@@ -142,7 +178,7 @@ function draw() {
     y += dy;
 }
 
-setInterval(draw, 10);
+var runningTheGame = setInterval(draw, 10);
 
 
 
